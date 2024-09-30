3ad9e886b1d88ebda08e77a4b3a1dee651cf2306
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-30 20:29:19 -0300

    feature: configuração da chave da api de imagens e requisição pros dados

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,28 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function App() {
+
   const [count, setCount] = useState(0)
+
+  const [query, setQuery] = useState("")
+  const [category, setCategory] = useState("")
+
+  const fetchData = async ({query, category}) => {
+
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY
+
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: {
+        client_id: apiKey
+      }
+    })
+
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <div className="container">
