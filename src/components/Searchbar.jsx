46a99371514d48f34171faac7c11cd8ae0a4440b
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-09-30 21:05:37 -0300

    feature: criação da barra de navegação

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Searchbar = () => {
-  return (
-    <div>Searchbar</div>
-  )
+
+    const categories = [
+        "Natureza",
+        "Pessoas",
+        "Tecnologia",
+        "Animais",
+        "Esportes"
+    ]
+
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Pesquisar fotos..."/>
+            <button>Pesquisar</button>
+            <select>
+                {categories.map((category) => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
+            </select>
+        </div>
+    )
 }
 
 export default Searchbar
